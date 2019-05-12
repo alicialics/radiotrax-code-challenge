ac28b1f49d3dc7ba196542e0159bcf3290bd3fba
zhuo li <aliciali666@gmail.com>
2019-05-11 21:19:39 -0700

    add login logout functionalities to to app page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,56 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Login } from "./components/Login";
 import { DeviceTable } from "./components/DeviceTable";
+import { Alert, Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 import "./App.css";
+import { UserContext } from "./UserContext";
+import { deviceOptionsSlice } from "./reducers/devicesSlice";
+
+import { auth } from "./auth";
 
 function App() {
-  return <DeviceTable />;
+  const [user, setUser] = useState("");
+  const isError = useSelector(state => state.deviceOptions.isError);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.logout();
+  });
+
+  const login = (user, password) => {
+    auth.login(user, password);
+    setUser(user);
+    dispatch(deviceOptionsSlice.actions.setIsError(false));
+  };
+
+  const logout = () => {
+    auth.logout();
+    setUser("");
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        login,
+        logout
+      }}
+    >
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Devices</NavbarBrand>
+        <Nav className="ml-auto" navbar>
+          {user && (
+            <NavItem>
+              <NavLink onClick={logout}>Log Out</NavLink>
+            </NavItem>
+          )}
+        </Nav>
+      </Navbar>
+      {isError && <Alert color="danager">Authorization Failed</Alert>}
+      {user && !isError ? <DeviceTable /> : <Login />}
+    </UserContext.Provider>
+  );
 }
 
 export default App;
